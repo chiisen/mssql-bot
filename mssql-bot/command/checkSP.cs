8aab847e3861eebc5d7c407f83fe768f089b1989
sam1 <chiisen@hotmail.com>
2024-10-01 12:07:46 +0800

    ✨ feat: 列出目前無權限的 SP Name

--- mssql-bot/command/checkSP.cs
+++ mssql-bot/command/checkSP.cs
@@ -233,7 +233,7 @@ public partial class Program
                 if(procedureCode.Length == 0)
                 {
                     // 沒有內容，可能是權限不足，不需要再處理了
-                    throw new Exception("權限不足，無法取得程式碼。");
+                    throw new Exception($"{procedureName} 權限不足，無法取得程式碼。");
                 }
                 list.Add(
                     new SPData { ROUTINE_NAME = procedureName, ROUTINE_DEFINITION = procedureCode }
